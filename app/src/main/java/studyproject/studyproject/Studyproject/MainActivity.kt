8b0b597cf4b3ec6c2053623d16b26be6a1f00e51
Shupegin <46937157+Shupegin@users.noreply.github.com>
2022-03-21 08:59:28 +0500

    new project

---
+++ app/src/main/java/studyproject/studyproject/Studyproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package studyproject.studyproject.Studyproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
